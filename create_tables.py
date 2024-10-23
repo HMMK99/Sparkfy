026319467d9230934bafeecc237543d54e8cc374
Hatem Kamal <44302506+HMMK99@users.noreply.github.com>
2024-10-23 13:46:36 +0300

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -10,8 +10,7 @@ def create_database():
     
     # connect to default database
     # conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
-    conn = psycopg2.connect(dsn = "postgresql://hmmk99:e45u1bUqazGrsuEGMa6NesSIXKNLputv@dpg-csbqo023esus73fvq8b0-a.oregon-postgres.render.com/xshopydb" \
-                         ,password="e45u1bUqazGrsuEGMa6NesSIXKNLputv")
+    conn = psycopg2.connect(dsn =)
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
@@ -23,8 +22,7 @@ def create_database():
     conn.close()    
     
     # connect to sparkify database
-    conn = psycopg2.connect(dsn = "postgresql://hmmk99:e45u1bUqazGrsuEGMa6NesSIXKNLputv@dpg-csbqo023esus73fvq8b0-a.oregon-postgres.render.com/sparkifydb" \
-                         ,password="e45u1bUqazGrsuEGMa6NesSIXKNLputv")
+    conn = psycopg2.connect(dsn = )
     cur = conn.cursor()
     
     return cur, conn
